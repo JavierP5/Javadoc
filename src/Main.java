e715cdc4837141eb1a30b4cdf9ef3e6267d3e929
javier <jportelagarcia@danielcastelao.org>
2024-12-11 14:22:10 +0100

    Javadoc v1.0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+/**
+ * Juego de Hundir la Flota
+ * @version 1.0
+ * @author Javier Portela García
+ */
 public class Main {
+
+    /**
+     * Función principal
+     * @param args parametros del juego
+     */
     public static void main(String[] args) {
 
         // definir
@@ -19,11 +29,15 @@ public class Main {
 
     }
 
+    /**
+     * Recorre el tablero de juego
+     * @param tablero array de dos dimensiones, el tablero con los barcos
+     */
     private static void recorrerTablero(int[][] tablero) {
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                if(tablero[i][j]>0){
-                    switch(tablero[i][j]){
+                if(esNave(tablero[i][j])){
+                    switch(tablero[i][j]) {
                         case 1:
                             System.out.println("Submarino " + i +":" + j);
                             break;
@@ -39,6 +53,42 @@ public class Main {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * Comprueba si el valor es una nave o no
+     *
+     * @param valor_coordenada valor de la coordenada a estudiar
+     * @return true en caso de ser una nave
+     */
+    static boolean esNave(int valor_coordenada){
+        return valor_coordenada>0;
+    }
+
+    /**
+     * Muestra por pantalla el tipo de nave segun valor coordenada
+     * también muestra la coordenada y la ordenada
+     * @param valor_coordenada valor de la coordenada
+     * @param x coordenada, eje horizontal
+     * @param y ordenada, eje vertical
+     */
+
+    static void tipoDeNave(int valor_coordenada, int x, int y){
+        switch (valor_coordenada) {
+            case 1:
+                System.out.println("Submarino " + x + ":" + y);
+                break;
+            case 2:
+                System.out.println("Acorazado " + x + ":" + y);
+                break;
+            case 4:
+                System.out.println("Portaaviones " + x + ":" + y);
+                break;
+            default:
+                System.out.println("No se lo que es");
+                break;
+                        
         }
     }
 }
